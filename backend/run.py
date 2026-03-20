f13c0404a65224be7833f6aac9aa548866a071db
opl77 <oliverperegrina77@gmail.com>
2026-03-20 15:12:12 -0600

    auto crear tablas al iniciar

--- backend/run.py
+++ backend/run.py
@@ -1,8 +1,10 @@
 ﻿import os
-from app import create_app, socketio
+from app import create_app, socketio, db
 
 app = create_app()
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     port = int(os.environ.get('PORT', 5000))
     socketio.run(app, host='0.0.0.0', port=port, debug=False)
